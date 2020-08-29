67198b2ace64487ea6142e30f28239340d5fde3b
dimitarmileski <dime.mileski@hotmail.com>
2020-08-29 02:01:55 +0200

    Flask CRUD control panel app

---
+++ basic-crud-flask-master/app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views
